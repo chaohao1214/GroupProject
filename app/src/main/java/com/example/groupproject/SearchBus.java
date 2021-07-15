24bd167f746333213e349a8cc69919402ea9924f
chaohao1214 <zch264999454@gmail.com>
2021-07-14 20:30:17 -0400

    Recycleview  finished

--- app/src/main/java/com/example/groupproject/SearchBus.java
+++ app/src/main/java/com/example/groupproject/SearchBus.java
@@ -7,14 +7,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import org.w3c.dom.Text;
-
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;

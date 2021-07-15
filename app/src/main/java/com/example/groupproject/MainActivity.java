97f716ab19b2413e96561431a975152e107ce2c3
Weiping Guo <guo00102@algonquinlive.com>
2021-07-15 00:09:09 -0400

    Step 1:Finished ImageButton and MainActivity draft.

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton movie = findViewById(R.id.movie_info);
+        movie.setOnClickListener(clk -> {
+            Intent movieFinder = new Intent(MainActivity.this, MovieInfo.class);
+            startActivity(movieFinder);
+        });
     }
 }
---
+++ app/src/main/java/com/example/groupproject/MovieInfo.java
@@ -0,0 +1,6 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MovieInfo extends AppCompatActivity {
+}

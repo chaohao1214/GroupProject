6dc7f6d989c99e163d4120a5d06efee96de433f5
salih <ensa0001@algonquinlive.com>
2021-07-18 10:36:20 -0400

    commit:  saving my work

---
+++ app/src/main/java/com/example/groupproject/RecyclerAdapter.java
@@ -0,0 +1,56 @@
+package com.example.groupproject;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+
+    private static final String TAG = "RecyclerAdapter";
+    List<String> itemList;
+
+    public RecyclerAdapter(List<String> infoList) {
+        this.itemList = infoList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.soccer_row_item, parent,false);
+        ViewHolder viewholder = new ViewHolder(view);
+        return viewholder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerAdapter.ViewHolder holder, int position) {
+        holder.title.setText(itemList.get(position));
+        //holder.value.setText(String.valueOf(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        TextView title, value;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+//            imageView = itemView.findViewById(R.id.imageView);
+//            title = itemView.findViewById(R.id.titleText);
+//            value = itemView.findViewById(R.id.valueText);
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/groupproject/SoccerActivityThird.java
+++ app/src/main/java/com/example/groupproject/SoccerActivityThird.java
@@ -1,17 +1,32 @@
 package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class SoccerActivityThird extends AppCompatActivity {
+    RecyclerView recyclerView;
+    RecyclerAdapter recyclerAdapter;
+
+    List<String> itemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.soccer_activity_third);
+
+        itemList = new LinkedList<>();
+
+        recyclerView = findViewById(R.id(R.id.recyclerView);
+
         Intent fromPrePage = getIntent();
+
+        itemList.add("list1 ");
 
     }
 }

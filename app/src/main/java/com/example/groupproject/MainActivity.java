fba1d1d25a0899d3f1e6889459337333abce74ee
Shakib Ahmed <ahme0498@algonquinlive.com>
2021-07-29 18:56:45 -0400

    "WIP-4: Milestone 2" 1. Added menubar in the main activity page.

--- app/src/main/java/com/example/groupproject/MainActivity.java
+++ app/src/main/java/com/example/groupproject/MainActivity.java
@@ -1,20 +1,28 @@
 package com.example.groupproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar main_menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button carCharge = findViewById(R.id.CarCharge);
+
+        main_menu = findViewById(R.id.carMenu);
+        setSupportActionBar(main_menu);
 
         carCharge.setOnClickListener(clk -> {
             // go to next page
@@ -23,4 +31,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void startCarChargingActivity() {
+        Intent chargingActivity = new Intent(this, SearchStation.class);
+        startActivity(chargingActivity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.car_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.car_charge_activity_opener:
+                startCarChargingActivity();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
